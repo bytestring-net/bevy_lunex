407d5e1f2c07b878b1f4eba198fb022cc1ee3816
IDEDARY <IDEDARY@gmail.com>
2023-06-27 14:28:30 +0200

    Update ui_core.rs

--- src/ui_core.rs
+++ src/ui_core.rs
@@ -127,7 +127,7 @@ impl Branch {
         let old = self.is_visible();
         self.visible = visible;
         let new = self.is_visible();
-        println!("({} != {}) {}", new, old, self.parent_visible);
+        println!("({} != {}) parent: {}", new, old, self.parent_visible);
         if new != old {
             self.cascade_visibility(new)
         }
