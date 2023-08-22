1af856d139a39a4faa537a09576acb28482c8b78
IDEDARY <IDEDARY@gmail.com>
2023-08-22 13:24:06 +0200

    added prelude

--- src/lib.rs
+++ src/lib.rs
@@ -12,5 +12,28 @@ pub use crate::core::{
 };
 
 pub mod prelude {
-    
+    pub use crate::{
+        textgrid,
+        textrow,
+
+        core::widget::Widget,
+        core::container::layout,
+        core::container::SolidScale,
+        core::container::LayoutPackage,
+
+        core::util::AsLunexVec2,
+        core::util::tween,
+        core::util::{tween_color_rgba, tween_color_hsla_short, tween_color_hsla_long},
+
+
+        utilities::export::{ImageParams, ImageElementBundle, TextParams, TextElementBundle, element_update},
+        utilities::export::{GridParams, grid_generate, grid_generate_inside},
+
+        core::tree::{UiTree, BranchError, Data, hierarchy_update},
+        utilities::export::{
+            LunexDebugPlugin,
+            Cursor,
+            cursor_update,
+        },
+    };
 }
