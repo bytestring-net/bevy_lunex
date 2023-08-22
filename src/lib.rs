3f9ed1f392ddcbc325a31593b8fab2e00baa8505
IDEDARY <IDEDARY@gmail.com>
2023-08-22 13:30:45 +0200

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -13,27 +13,24 @@ pub use crate::core::{
 
 pub mod prelude {
     pub use crate::{
+
+        // Macros export
         textgrid,
         textrow,
-
+        
+        // Export core
         core::widget::Widget,
-        core::container::layout,
-        core::container::SolidScale,
-        core::container::LayoutPackage,
+        core::container::{layout, SolidScale, LayoutPackage},
+        core::tree::{UiTree, BranchError, Data, hierarchy_update},
 
+        // Some of it will get moved to Mathia crate
         core::util::AsLunexVec2,
         core::util::tween,
         core::util::{tween_color_rgba, tween_color_hsla_short, tween_color_hsla_long},
 
-
+        // Reorganize in the future
         utilities::export::{ImageParams, ImageElementBundle, TextParams, TextElementBundle, element_update},
         utilities::export::{GridParams, grid_generate, grid_generate_inside},
-
-        core::tree::{UiTree, BranchError, Data, hierarchy_update},
-        utilities::export::{
-            LunexDebugPlugin,
-            Cursor,
-            cursor_update,
-        },
+        utilities::export::{LunexDebugPlugin, Cursor, cursor_update},
     };
 }
