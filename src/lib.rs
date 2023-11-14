c8eeb09e8fc02532899e236cdc8829719b597edb
IDEDARY <IDEDARY@gmail.com>
2023-11-14 17:37:18 +0100

    Small change

--- src/lib.rs
+++ src/lib.rs
@@ -176,7 +176,7 @@
 
 pub mod prelude {
     pub use bevy_lunex_core::prelude::*;
-    pub use bevy_lunex_macros::*;
+    //pub use bevy_lunex_macros::*;
     pub use bevy_lunex_ui::prelude::*;
     pub use bevy_lunex_utility::prelude::*; 
 }
